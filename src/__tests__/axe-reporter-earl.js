ad1f8b198c51feab78cf94e64891cb077711b281
jkodu <jey.nandakumar@gmail.com>
2019-08-05 17:17:18 +0100

    test: earlUntested

--- src/__tests__/axe-reporter-earl.js
+++ src/__tests__/axe-reporter-earl.js
@@ -1,86 +1,7 @@
-const { axeReporterEarl } = require('./../axe-reporter-earl')
+const { axeReporterEarl, earlUntested } = require('./../axe-reporter-earl')
 const context = require('./../context')
 
-const rawResults = [
-	[
-		{
-			id: 'autocomplete-valid',
-			result: 'passed',
-			pageLevel: false,
-			impact: null,
-			tags: ['cat.forms', 'wcag21aa', 'wcag135'],
-			description: 'Ensure the autocomplete attribute is correct and suitable for the form field',
-			help: 'autocomplete attribute must be used correctly',
-			helpUrl: 'https://dequeuniversity.com/rules/axe/3.2/autocomplete-valid?application=axe-puppeteer',
-			inapplicable: [],
-			passes: [
-				{
-					any: [],
-					all: [
-						{
-							id: 'autocomplete-valid',
-							data: null,
-							relatedNodes: [],
-							impact: 'serious',
-							message: 'the autocomplete attribute is correctly formatted',
-						},
-						{
-							id: 'autocomplete-appropriate',
-							data: null,
-							relatedNodes: [],
-							impact: 'serious',
-							message: 'the autocomplete value is on an appropriate element',
-						},
-					],
-					none: [],
-					node: {
-						_fromFrame: false,
-						spec: {},
-						source: '<input autocomplete="section-primary shipping work email">',
-						_element: {},
-					},
-					impact: null,
-					result: 'passed',
-				},
-			],
-			incomplete: [],
-			violations: [],
-		},
-	],
-	[
-		{
-			id: 'autocomplete-valid',
-			result: 'failed',
-			pageLevel: false,
-			impact: 'serious',
-			tags: ['cat.forms', 'wcag21aa', 'wcag135'],
-			description: 'Ensure the autocomplete attribute is correct and suitable for the form field',
-			help: 'autocomplete attribute must be used correctly',
-			helpUrl: 'https://dequeuniversity.com/rules/axe/3.2/autocomplete-valid?application=axe-puppeteer',
-			inapplicable: [],
-			passes: [],
-			incomplete: [],
-			violations: [
-				{
-					any: [],
-					all: [
-						{
-							id: 'autocomplete-valid',
-							data: null,
-							relatedNodes: [],
-							impact: 'serious',
-							message: 'the autocomplete attribute is incorrectly formatted',
-						},
-					],
-					none: [],
-					node: { _fromFrame: false, spec: {}, source: '<input autocomplete="badterm">', _element: {} },
-					impact: 'serious',
-					result: 'failed',
-				},
-			],
-		},
-	],
-]
+const rawResults = require('./data/raw-results-73f2c2.json')
 
 describe(`axe-reporter-earl`, () => {
 	describe(`axeReporterEarl fn`, () => {
@@ -102,7 +23,6 @@ describe(`axe-reporter-earl`, () => {
 					version: '100.200.999',
 				},
 			}
-
 			const actual = axeReporterEarl(args)
 
 			expect(actual).toContainAllKeys(['@context', '@graph'])
@@ -118,13 +38,35 @@ describe(`axe-reporter-earl`, () => {
 			expect(assertion['mode']).toBeOneOf([
 				'earl:automatic', // note: can be extended later
 			])
-			expect(assertion.assertedBy).toBe(`https://github.com/dequelabs/axe-core/releases/tag/${args.env.version}`)
-
+			expect(assertion.assertedBy).toEndWith(`${args.env.version}`)
 			expect(assertion.result.outcome).toBe(`earl:${rawResult.result}`)
 		})
 	})
 
-	describe(`earlUntested fn`, () => {})
+	describe(`earlUntested fn`, () => {
+
+    test(`get assertion of untested testcase`, () => {
+      const args = { url: 'http://idonot.exist', version: '100.200.999' }
+      const actual = earlUntested(args)
+
+      expect(actual).toContainAllKeys(['@context', '@graph'])
+
+      expect(actual['@context']).toBeObject()
+			expect(JSON.stringify(actual['@context'])).toEqual(JSON.stringify(context))
+
+      expect(actual['@graph'].length).toBe(1)
+
+      const assertion = actual['@graph'][0]
+			expect(assertion).toContainAllKeys(['@type', 'mode', 'subject', 'assertedBy', 'result'])
+			expect(assertion['@type']).toBe('Assertion')
+			expect(assertion['mode']).toBeOneOf([
+				'earl:automatic', // note: can be extended later
+      ])
+      expect(assertion.assertedBy).toEndWith(`${args.version}`)
+      expect(assertion.result.outcome).toBe(`earl:untested`)
+    })
+
+  })
 
 	// describe(`concatReport`, () => {
 	// })
