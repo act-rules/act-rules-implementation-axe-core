c5f72e94e2dc5fbb90474e8ebc5d3c37adc14e1b
Jey Nandakumar <jey.nandakumar@gmail.com>
2019-12-31 21:22:45 +0000

    fix: run correct axe rules for testcase (#10)
    * use axe-core#wai-tools branch
    
    * add prebuild step to build axe
    
    * helper npm script and update gitignore
    
    * fix: ignore by ruleId and ignore by file extension
    
    * fix: assertions for inapplicable file extensions
    
    * fix: run correct axe rules using options over withTags

--- src/axe-runner.js
+++ src/axe-runner.js
@@ -1,18 +1,72 @@
 const { readFileSync } = require('fs')
 const path = require('path')
 const { AxePuppeteer } = require('axe-puppeteer')
-const { axeReporterEarl, earlUntested } = require('./axe-reporter-earl')
-const { version } = require('axe-core')
+const { axeReporterEarl, earlUntested, earlInapplicable, inapplicableFileExtnensions, ignoreRulesIds } = require('./axe-reporter-earl')
+const axe = require('axe-core')
 const axeSource = readFileSync(require.resolve('axe-core'), 'utf-8')
 
-const ignoreRules = [
-	'Meta-refresh no delay', // TODO: Figure out why these TCs throw
-]
+/**
+ * Run axe-pupppeteer in a given page, with a success criterion
+ */
+const axeRunner = async (page, { url = '', ruleSuccessCriterion: tags = [], ruleId, getSourceUrl }) => {
+	// check if running axe should be ignored
+	const extn = getFileExtension(url)
+	const env = {
+		url: getSourceUrl(url),
+		version: axe.version
+	}
 
-const ignoreFileTypes = [
-	'svg',
-	'xml'
-]
+	if (ignoreRulesIds.includes(ruleId) || tags.length === 0) {
+		return earlUntested(env)
+	}
+
+	// Get the page and make sure it loads correctly
+	await Promise.race([page.goto(url), page.waitFor('body')])
+
+	// if given page is of type `html`, ensure it loaded
+	if (extn === `html`) {
+		const html = await page.$eval('html', e => e.outerHTML)
+		if (html.includes('Not Found')) {
+			console.log(`Not Found ${url}`)
+			return earlUntested(env)
+		}
+	}
+
+	// return
+	return Promise.race([
+		analyze(), // Run axe to completion
+		timeoutReject(50000, `Timeout for page ${url}`), // or, timeout after 5s
+	])
+
+	/* Run axe and return EARL */
+	async function analyze() {
+
+		try {
+			let raw
+			const axeRules = axe.getRules(tags) || []
+			const axeRulesIds = axeRules.map(({ ruleId }) => ruleId)
+
+			// check for inapplicable file extensions
+			if (inapplicableFileExtnensions.includes(extn)) {
+				raw = axeRulesIds.map(ruleId => {
+					return { result: `inapplicable`, id: ruleId }
+				})
+			} else {
+				// Setup axe-puppeteer with the correct SC
+				raw = await new AxePuppeteer(page, axeSource)
+					.options({ runOnly: { type: 'rule', values: axeRulesIds }, reporter: 'raw' })
+					.analyze()
+			}
+
+			return axeReporterEarl({ raw, env })
+		} catch (error) {
+			console.error(error)
+		}
+	}
+}
+
+module.exports = axeRunner
+
 
 /* Reject with a message after a certain time */
 function timeoutReject(t, msg) {
@@ -21,63 +75,6 @@ function timeoutReject(t, msg) {
 	})
 }
 
-/**
- * Run axe-pupppeteer in a given page, with a success criterion
- */
-const axeRunner = async (page, args) => {
-	const { url = '', ruleSuccessCriterion: tags, ruleName, getSourceUrl } = args
-
-	// Work out if axe knows how to test this page
-	if (ignoreRules.includes(ruleName)) {
-		return earlUntested({ url, version })
-	}
-
-	const extn = getFileExtension(url)
-	if (extn && ignoreFileTypes.includes(extn)) {
-		return earlUntested({ url, version })
-	}
-
-	if (tags.length === 0) {
-		return earlUntested({ url, version })
-	}
-
-	// Get the page and make sure it loads correctly
-	await Promise.race([page.goto(url), page.waitFor('body')])
-
-	const html = await page.$eval('html', e => e.outerHTML)
-
-	if (html.includes('Not Found')) {
-		console.log(`Not Found ${url}`)
-		return earlUntested({ url, version })
-	}
-
-	// Setup axe-puppeteer with the correct SC
-	const axeRunner = new AxePuppeteer(page, axeSource)
-	axeRunner.options({ reporter: 'raw' })
-	axeRunner.withTags(tags)
-
-	/* Run axe and return EARL */
-	async function analyze() {
-		const raw = await axeRunner.analyze()
-		return axeReporterEarl({
-			raw,
-			env: {
-				url: getSourceUrl(url),
-				version,
-			},
-		})
-	}
-
-	return Promise.race([
-		// Run axe to completion
-		analyze(),
-		// or, timeout after 5s
-		timeoutReject(5000, `Timeout for page ${url}`),
-	])
-}
-
-module.exports = axeRunner
-
 
 /**
  * Get extension from path
@@ -85,5 +82,6 @@ module.exports = axeRunner
  */
 function getFileExtension(str) {
 	const filename = path.basename(str);
-	return path.extname(filename).slice(1);
+	const extn = path.extname(filename).slice(1)
+	return extn || ""
 }
--- src/run-rule-tests.js
+++ src/run-rule-tests.js
@@ -11,14 +11,20 @@ const runRuleTests = async (pageRunner, testcases, port, siteUrl) => {
 			return
 		}
 
-		const ruleScs = Object.keys(ruleAccessibilityRequirements)
-			.filter(key => key.includes(`wcag`))
-			.map(key => key.split(':').pop())
-			.map(sc => 'wcag' + sc.replace(/\./g, ''))
+		const wcagAccReqs = Object.keys(ruleAccessibilityRequirements).filter(key => {
+			return key.includes('wcag20') || key.includes('wcag21')
+		})
 
+		const ruleScs = wcagAccReqs.map(key => key.split(':').pop()).map(sc => 'wcag' + sc.replace(/\./g, ''))
+		if (!ruleScs || !ruleScs.length) {
+			continue
+		}
+
+		const { ruleId, ruleName, relativePath } = tc
 		const results = await pageRunner({
-			url: `http://127.0.0.1:${port}/${tc.relativePath}`,
-			ruleName: tc.ruleName,
+			ruleId,
+			ruleName,
+			url: `http://127.0.0.1:${port}/${relativePath}`,
 			ruleSuccessCriterion: ruleScs,
 			getSourceUrl: assertionUrl => {
 				if (!siteUrl) {
