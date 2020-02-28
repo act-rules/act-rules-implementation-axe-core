474708c42532f696a1cbeb2ccf8b3a53ae038c8c
jkodu <jey.nandakumar@gmail.com>
2020-02-28 15:15:39 +0000

    fix: do not ignore invalid role

--- src/config/rules-axe-options.js
+++ src/config/rules-axe-options.js
@@ -4,12 +4,10 @@ const rulesAxeOptions = {
    * Url: https://act-rules.github.io/rules/674b10
    *  - Disable below checks
    *    - `fallbackrole`
-   *    - `invalidrole`
    */
   '674b10': {
     checks: {
-      'fallbackrole': { enabled: false },
-      'invalidrole': { enabled: false }
+      'fallbackrole': { enabled: false }
     }
   }
 }
