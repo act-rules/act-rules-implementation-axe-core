306b5665b727b78195cf8731378d605a80517cdd
Wilco Fiers <wilco.fiers@deque.com>
2020-08-05 13:33:43 +0200

    feat: use outfile as a cache

--- src/run-testcases.js
+++ src/run-testcases.js
@@ -1,13 +1,14 @@
+const fs = require('fs')
+const path = require('path')
 const staticServer = require('static-server')
 
 const groupedTestcases = require('./group-testcases')
 const runRuleTests = require('./run-rule-tests')
 
-const runTestcases = async (options, pageRunner) => {
-	const { port = 1339, testsDir, testsJson, ruleId = undefined, siteUrl } = options
-
+const runTestcases = async (program, pageRunner) => {
+	const { port = 1339, testsDir, testsJson, ruleId = undefined, siteUrl } = program
+	const cacheResults = getCacheResults(program)
 	const tests = groupedTestcases(testsJson, ruleId)
-
 	const server = new staticServer({
 		rootPath: testsDir,
 		port,
@@ -16,22 +17,44 @@ const runTestcases = async (options, pageRunner) => {
 	server.start()
 
 	const results = []
-	for (let ruleTest of tests) {
-		const { ruleName, ruleId } = ruleTest[0]
-
+	for (let testcases of tests) {
+		const { ruleName, ruleId } = testcases[0]
 		console.log(`Testing: ${ruleName} (${ruleId})`)
 		process.stdout.write('  ')
+
+		const untestedCases = []
+		testcases.forEach((testcase) => {
+			const testcaseCache = cacheResults.filter(({ subject }) => {
+				return subject.source.includes(testcase.relativePath)
+			}).map(assertion => ({ '@graph': [assertion]}))
+
+			if (testcaseCache.length > 0) {
+				results.push(...testcaseCache)
+			} else {
+				untestedCases.push(testcase)
+			}
+		})
+		// Show cached results
+		process.stdout.write(','.repeat(testcases.length - untestedCases.length))
+		
 		try {
-			const ruleResults = await runRuleTests(pageRunner, ruleTest, port, siteUrl)
+			const ruleResults = await runRuleTests(pageRunner, untestedCases, port, siteUrl)
 			if (ruleResults && ruleResults.length > 0) {
 				results.push(...ruleResults)
 			}
 		} catch (e) {
 			console.error(e.message, e.stack)
-		}
+		}	
 	}
 	server.stop()
 	return results
+}
+
+function getCacheResults({ outfile }) {
+	if (fs.existsSync(outfile)) {
+		return require(path.resolve(outfile))['@graph']
+	}
+	return []
 }
 
 module.exports = runTestcases
